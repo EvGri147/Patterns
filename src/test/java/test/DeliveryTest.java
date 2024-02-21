a00005092781f409fda7862e587407211a18b565
EvGri <evgeniigribanov@mail.ru>
2024-02-21 20:45:29 +0600

    Add Tests

---
+++ src/test/java/test/DeliveryTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import com.codeborne.selenide.Condition;
+import data.DataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliveryTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[placeholder=\"Город\"]").setValue(validUser.getCity());
+        $("[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(firstMeetingDate);
+        $("[data-test-id=\"name\"] input.input__control").setValue(validUser.getName());
+        $("[name=\"phone\"]").setValue(validUser.getPhone());
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $("[type=button] .button__text").click();
+        $(".notification__title").shouldBe(Condition.visible, Duration.ofSeconds(4));
+        $("[data-test-id=success-notification] .notification__content").should(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(secondMeetingDate);
+        $("[type=button] .button__text").click();
+        $("[data-test-id=replan-notification] .notification__title").should(Condition.exactText("Необходимо подтверждение"));
+        $("[data-test-id=replan-notification] span.button__text").click();
+        $("[data-test-id=success-notification] .notification__content").should(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
+}
